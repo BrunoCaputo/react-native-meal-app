86bbd48cc1fcf9c5e6159b83c53b7e9a8c5f89bd
BrunoCaputo <brunopc.dev@gmail.com>
2023-08-25 19:45:34 -0300

    feat: Improve navigation

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -2,12 +2,11 @@ import { FlatList, ListRenderItemInfo } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import Category from "../models/category";
 import CategoryGridTile from "../components/CategoryGridTile";
-import { NavigationProp } from "@react-navigation/native";
 
 function CategoriesScreen({ navigation }: any) {
   function renderCategoryItem({ item }: ListRenderItemInfo<Category>) {
     function pressHandler() {
-      navigation.navigate("MealsOverview");
+      navigation.navigate("MealsOverview", { categoryId: item.id });
     }
 
     return (
--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,9 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  ListRenderItemInfo,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { MEALS } from "../data/dummy-data";
+import Meal from "../models/meal";
+import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }: any) {
+  const categoryId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  function renderMealItem({ item }: ListRenderItemInfo<Meal>) {
+    return <MealItem title={item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
